452a1df9569e31d9b4fa30f8e7533b5bbde05618
lKilgoretrout <lkilgoretrout@yahoo.com>
2021-04-21 12:54:50 -0700

    Update student.js

--- hw3Comparator/student.js
+++ hw3Comparator/student.js
@@ -1,6 +1,3 @@
-const DEBUG_MODE = true;
-if (DEBUG_MODE) {debugger;}
-
 
 // You are not permitted to change this in any way
 function Student(name, major, yearInSchool, club) {
@@ -8,7 +5,7 @@ function Student(name, major, yearInSchool, club) {
   this.major = major; // string, (e.g. "Computer Science", "Art", "Business")
   this.yearInSchool = yearInSchool; // int, (e.g. 1, 2, 3, 4)
   this.club = club; // string, (e.g. "Improv", "Art")
-}
+};
 
 var students = [
   new Student("Pam", "Art", 2, "Art"),
@@ -17,10 +14,11 @@ var students = [
   new Student("Jim", "Sports Science", 2, "Guitar"),
   new Student("Angela", "Accounting", 4, "Cat"),
   new Student("Toby", "Human Resources", 3, "Photography")
+
 ];
 
 // add the logMe function to the Student prototype:
-Student.prototype.logMe = function() {
+Student.prototype.logMe = function(showClub) {
 	/** 
 	 * prints Student attributes, 
 	 * if showClub==false, doesn't print club attribute
@@ -29,61 +27,51 @@ Student.prototype.logMe = function() {
 	
 		returnString = ""
 		range = (showClub ? 4 : 3); // if showClub --> show all 4 attributes
+		
 		for (i=0; i<range; i++)		
 		{
-			returnString += Object.values(Student)[i] + " - ";
+			returnString += Object.values(this)[i] + " - ";
 		}
+	    // trim the trailing " - " from return value
+		returnString = returnString.substring(0, returnString.length - 3);
+		return returnString;
 };
 	
 /* This function sorts arrays using an arbitrary comparator. You pass it a comparator 
 and an array of objects appropriate for that comparator and it will return a new array 
 which is sorted with the largest object in index 0 and the smallest in the last index*/
 
-// This implementation of Merge Sort inspired by Abhilash Kakumanu's verions at
-//  https://stackabuse.com/merge-sort-in-javascript/
-function mergeSort(comparator, array) 
+// This implementation of Bubble Sort inspired by Amir Popovich verions at
+//  https://stackoverflow.com/questions/37817334/javascript-bubble-sort
+function bubbleSort(comparator, arr) 
 {
-  const half = array.length / 2;
-  
-  // Base case or terminating case
-  if (array.length < 2)
-  {
-    return array;
-  }
-  
-  const left = array.splice(0, half)
-  return merge(comparator, mergeSort(left), mergeSort(array))
+    var len = arr.length;
+    
+    for (var i = 0; i < len ; i++) 
+	{
+        for(var j = 0 ; j < len - i - 1; j++)
+		{ 
+			
+			if (comparator(arr[j], arr[j+1]))
+			{
+			  var temp = arr[j];
+			  arr[j] = arr[j+1];
+			  arr[j + 1] = temp;
+			}
+		}
+    }
+    return arr;
 }
 
-// helper function for mergeSort
-function merge(comparator, left, right) 
-{
-    let arr = []
-    
-    while (left.length && right.length) {
-        
-		// drop that sweet custom comparator right in there
-        if (comparator(right[0], left[0]) ) 
-		{
-            arr.push(left.shift())  
-        } 
-		else 
-		{
-            arr.push(right.shift()) 
-        }
-    }
-    
-    // Concatenating the leftover elements
-    // (in case we didn't go through the entire left or right array)
-    return [ ...arr, ...left, ...right ]
-}
+
+
 
 /* A comparator takes two arguments and uses some algorithm to compare them. If the first 
 argument is larger or greater than the 2nd it returns true, otherwise it returns false.
 Here is an example that works on integers*/
 function exComparator( int1, int2)
 {
-    if (int1 > int2) 
+    if (int1 < int2) 
 	{
         return true;
     } 
@@ -91,7 +79,7 @@ function exComparator( int1, int2)
 	{
       return false;
     }
-}
+};
 
 /* For all comparators if students are 'tied' according to the comparison rules then the order of 
 those 'tied' students is not specified and either can come first*/
@@ -99,7 +87,7 @@ those 'tied' students is not specified and either can come first*/
 /* This compares two students based on their year in school. Sort in descending order.*/
 function yearComparator(student1, student2) 
 {
-    if (Student1.yearInSchool > Student2.yearInSchool) 
+    if (Student1.yearInSchool < Student2.yearInSchool) 
 	{
 	    return true;
     }
@@ -107,22 +95,22 @@ function yearComparator(student1, student2)
     {
         return false
     }		
-}
+};
 
 /* This compares two students based on their major. It should be case insensitive and 
 makes which are alphabetically earlier in the alphabet are "greater" than ones that 
 come later (from A-Z).*/
 function majorComparator(student1, student2) 
 {
-    if (Student1.major.toLowerCase() > Student2.major.toLowerCase()) 
+    if (student1.major.toLowerCase() < student2.major.toLowerCase()) 
 	{
 	    return true;
     }
     else
     {
-        return false
+        return false;
     }		
-}
+};
 
 /* This compares two students based on the club they're in. The ordering from "greatest" 
 to "least" is as follows: improv, cat, art, guitar, (types not otherwise listed). 
@@ -144,19 +132,21 @@ function clubComparator(student1, student2)
 	else if  (s1club === s2club) {       // if club is equal, sort by yearInSchool
 		 if (Student1.yearInSchool > Student2.yearInSchool)                 {return true;}
 		 else																{return false;}
-}
-
+	}
+};
 
 function main()
 {
-    var studentsByMajor = mergeSort(majorComparator, students)
-	console.log("**********")
-	for (var student in studentsByMajor)
+    var studentsByMajor = bubbleSort(majorComparator, students);
+	console.log(studentsByMajor);
+	console.log("**********");
+	for (var student of studentsByMajor)
 	{
-		student.logMe();
+		console.log(student.logMe(showClub=false));
 	}
-}
-		
+};
+	
+main();	
 // Your program should output the following to the console.log, including each of the opening and closing 
 //stars. All values in parenthesis should be replaced with appropriate values. To accomplish this, you will 
 //have to sort the array of students using each comparator and then loop through the array and and call logMe
